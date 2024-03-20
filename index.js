bf201a0f48347274a14d9bc6f3430cb33259b815
Cindy <cindy.mbishmutisya@gmail.com>
2024-03-21 00:11:33 +0300

    arithmetics lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+let random = 6;
+const num3 = 80;
+const num4 = 20;
+const mod = num3 / num4;
+
+
+const max = Math.max(12,13,4,20);
+
